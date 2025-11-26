7ceaaffb5246c69df7b70f838f6cd934c0a01f06
eduarda-martins0204 <145498651+eduarda-martins0204@users.noreply.github.com>
2025-11-25 23:05:31 -0300

    Add files via upload
    ajustes no codigo de controller.php

--- generic/Controller.php
+++ generic/Controller.php
@@ -1,38 +1,23 @@
 <?php
 namespace generic;
 
-class Controller {
-    private $arrChamadas = [];
-
-    public function __construct() {
-        $this->arrChamadas = [
-            "plantas/lista" => new Acao("Plantas", "listar"),
-            "plantas/formulario" => new Acao("Plantas", "formulario"),
-            "plantas/inserir" => new Acao("Plantas", "inserir"),
-            "plantas/formularioalterar" => new Acao("Plantas", "alterarForm"),
-            "plantas/alterar" => new Acao("Plantas", "alterar"),
-            "plantas/excluir" => new Acao("Plantas", "excluir"),
-            "usuarios/lista" => new Acao("Usuarios", "listar"),
-            "usuarios/formulario" => new Acao("Usuarios", "formulario"),
-            "usuarios/inserir" => new Acao("Usuarios", "inserir"),
-            "usuarios/formularioalterar" => new Acao("Usuarios", "alterarForm"),
-            "usuarios/alterar" => new Acao("Usuarios", "alterar"),
-            "usuarios/excluir" => new Acao("Usuarios", "excluir"),
-            "cuidados/lista" => new Acao("Cuidados", "listar"),
-            "cuidados/formulario" => new Acao("Cuidados", "formulario"),
-            "cuidados/inserir" => new Acao("Cuidados", "inserir"),
-            "cuidados/formularioalterar" => new Acao("Cuidados", "alterarForm"),
-            "cuidados/alterar" => new Acao("Cuidados", "alterar"),
-            "cuidados/excluir" => new Acao("Cuidados", "excluir")
-        ];
+class Controller 
+{
+    private $rotas = null;
+    public function __construct()
+    {
+        $this->rotas = new Rotas();
     }
 
-    public function verificarChamadas($rota) {
-        if (isset($this->arrChamadas[$rota])) {
-            $acao = $this->arrChamadas[$rota];
-            $acao->executar();
-            return;
+    public function verificarChamadas($rota)
+    {
+        $retorno = $this->rotas->executar($rota);
+        
+        if ($retorno){
+            header("Content-Type: application/json");
+            $json = json_encode($retorno);
+            echo $json;
         }
-        echo "Rota não existe!";
     }
 }
+    
