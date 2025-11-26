ec3ea99754aadbfdc6dcb2921bfcf90b3e4276cf
eduarda-martins0204 <145498651+eduarda-martins0204@users.noreply.github.com>
2025-11-25 23:06:26 -0300

    Add files via upload
    ajustes no codigo de acao.php

--- generic/Acao.php
+++ generic/Acao.php
@@ -1,17 +1,83 @@
 <?php
 namespace generic;
 
-class Acao {
-    private $classe;
-    private $metodo;
+use ReflectionMethod;
 
-    public function __construct($classe, $metodo) {
-        $this->classe = "controller\\" . $classe;
-        $this->metodo = $metodo;
+class Acao
+{
+    const POST = "POST";
+    const GET = "GET";
+    const PUT = "PUT";
+    const PATCH = "PATCH";
+    const DELETE = "DELETE";
+
+    private $endpoint;
+
+    public function __construct($endpoint = [])
+    {
+        $this->endpoint = $endpoint;
     }
 
-    public function executar() {
-        $obj = new $this->classe();
-        $obj->{$this->metodo}();
+    public function executar()
+    {
+        $end = $this->endpointMetodo();
+
+        if ($end) {
+            $reflectMetodo = new ReflectionMethod($end->classe,$end->execucao);
+            $parametros = $reflectMetodo->getParameters();
+            $returnParam = $this->getParam();
+            if($parametros){
+                $para=[];
+                foreach($parametros as $v){
+                        $name = $v->getName();
+
+                        if(!isset($returnParam[$name])){
+                            return false;
+                        }
+                        $para[$name] = $returnParam[$name];
+                }
+               return $reflectMetodo->invokeArgs(new $end->classe(),$para);
+
+            }
+            return $reflectMetodo->invoke(new $end->classe());
+
+        }
+        return null;
+    }
+
+    private function endpointMetodo()
+    {
+        return isset($this->endpoint [$_SERVER["REQUEST_METHOD"]]) ? $this -> endpoint[$S_SERVER["REQUEST_METHOD"]] : null;
+    }
+
+    private function getPost(){
+        if($_POST){
+            return $_POST;
+        }
+        return [];
+    }
+
+    private function getGet(){
+        if ($_GET){
+            $get = $_GET;
+            unset($get["param"]);
+            return $get;
+        }
+        return [];
+    }
+
+    private function getInput(){
+        $input = file_get_contents("php://input");
+
+        if ($input){
+
+            return json_decode($input,true);
+        }
+        return[];
+
+    }
+
+    public function getParam(){
+        return array_merge($this->getPost(),$this->getGet(),$this->getInput());
     }
 }
