70f81ee37172791d8de5c9fd9569b2b6190a3afb
eduarda-martins0204 <145498651+eduarda-martins0204@users.noreply.github.com>
2025-11-27 00:04:11 -0300

    Add files via upload
    Ajuste em usuarios.php

--- controller/Usuarios.php
+++ controller/Usuarios.php
@@ -1,63 +1,37 @@
 <?php
+
 namespace controller;
 
 use service\UsuarioService;
 use template\UsuarioTemp;
 use template\ITemplate;
-use service\CuidadoService;
 
-class Usuarios {
-    private ITemplate $template;
 
-    public function __construct() {
-        $this->template = new UsuarioTemp();
+class Usuarios 
+{
+    public function __construct()
+    {
+
     }
 
-    public function listar() {
+    public function listar()
+    {
         $service = new UsuarioService();
-        $resultado = $service->listarUsuarios();
-        $this->template->layout("\\public\\usuarios\\listar.php", $resultado);
+        $resultado = $service->listarCliente();
+        return $resultado;
     }
 
-    public function inserir() {
-        $nome = $_POST["nome"];
-        $email = $_POST["email"];
+    public function inserir ($nome, $endereco)
+    {
         $service = new UsuarioService();
-        $resultado = $service->inserir($nome, $email);
-        header("location: /sistema_plantas/usuarios/lista?info=1");
+        $resultado = $service->inserir($nome, $endereco);
+        return $resultado;
     }
 
-    public function formulario() {
-        $this->template->layout("\\public\\usuarios\\form.php");
+    public function teste($nome, $telefone)
+    {
+        return "$nome,$telefone";
     }
-
-    public function alterarForm() {
-        $id = $_GET["id"];
-        $service = new UsuarioService();
-        $resultado = $service->listarId($id);
-        $this->template->layout("\\public\\usuarios\\form.php", $resultado);
-    }
-
-    public function alterar() {
-        $id = $_POST["id"];
-        $nome = $_POST["nome"];
-        $email = $_POST["email"];
-        $service = new UsuarioService();
-        $service->alterar($id, $nome, $email);
-        header("location: /sistema_plantas/usuarios/lista?info=2");
-    }
-    
-    public function excluir() {
-        $id = $_GET["id"];
-        $service = new UsuarioService();
-
-        // Instancia o serviço de cuidados para excluir os cuidados do usuário primeiro
-        $cuidadoService = new CuidadoService();
-        $cuidadoService->excluirPorUsuarioId($id);
-
-        // Agora o usuário pode ser excluído
-        $service->excluir($id);
-
-        header("location: /sistema_plantas/usuarios/lista?info=3");
-    }
+    public function teste2() {}
 }
+    
